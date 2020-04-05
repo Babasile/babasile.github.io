9447334c69adf7ae6ed6cf2ae644ac19c6f931ad
Babasile <Babasile@users.noreply.github.com>
2020-04-06 00:46:19 +0200

    Updates

--- precache-manifest.7858e4bef2b5428f10c52fcf835a41d3.js
+++ precache-manifest.7858e4bef2b5428f10c52fcf835a41d3.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "70195d052e595ac28ac8d98a3ca8a031",
+    "revision": "b24aa636b883366fa34cd5e93d2a7e21",
     "url": "/index.html"
   },
   {
-    "revision": "cfa6d294c03ee2ad317d",
+    "revision": "9949e13c46b2192d82ee",
     "url": "/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "e0d58df01f1efc13d50c",
-    "url": "/static/css/main.68fc79ca.chunk.css"
+    "revision": "09e2a389f6b99e1b4bab",
+    "url": "/static/css/main.73c82d4c.chunk.css"
   },
   {
-    "revision": "cfa6d294c03ee2ad317d",
-    "url": "/static/js/2.bae8b481.chunk.js"
+    "revision": "9949e13c46b2192d82ee",
+    "url": "/static/js/2.da9ed719.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/static/js/2.bae8b481.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.da9ed719.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e0d58df01f1efc13d50c",
-    "url": "/static/js/main.a01db5c3.chunk.js"
+    "revision": "09e2a389f6b99e1b4bab",
+    "url": "/static/js/main.87f40c5e.chunk.js"
   },
   {
     "revision": "70319b528d1ba637349d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6b864644b2a4f1f07a654f32f4578055.js"
+  "/precache-manifest.7858e4bef2b5428f10c52fcf835a41d3.js"
 );
 
 self.addEventListener('message', (event) => {
