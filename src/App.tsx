99bfbc3b4c230cafdf1396f7b9566ead718e1375
Babasile <Babasile@users.noreply.github.com>
2021-12-10 12:57:58 +0100

    Add missing import for FontAwesome Icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactGA from "react-ga";
 import "./App.css";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
-
+import { far } from "@fortawesome/free-regular-svg-icons";
 import SinglePage from "./components/single-page/single-page";
-
 import {
 	ApolloClient,
 	HttpLink,
@@ -17,6 +15,8 @@ import {
 } from "@apollo/client";
 import {I18nextProvider} from 'react-i18next';
 import i18n from './i18n/config';
+
+library.add(fab, fas, far);
 
 function App() {
 	if (process.env.NODE_ENV === "production") {
@@ -28,7 +28,6 @@ function App() {
 			uri: process.env.REACT_APP_GRAPHQL_URL,
 		}),
 	});
-	library.add(fab, fas);
 	return (
 		<I18nextProvider i18n={i18n}>
 			<ApolloProvider client={client}>
